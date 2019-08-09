a66a9dc78237b447b654576c6e6b3bb179b13f02
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-08-09 14:02:47 -0700

    adding to quantities table works now

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.get('/packages', (req, res) => {
+
+})
+
 app.post('/packages', (req, res) => {
   var data = req.body;
   var name = data.name;
@@ -20,7 +24,16 @@ app.post('/packages', (req, res) => {
     if (err) {
       console.log(err)
     } else {
-      console.log('successfully added to packages table!')
+      console.log('successfully added to packages table!');
+      for (var i = 0; i < quantities.length; i++) {
+        connection.query(`INSERT INTO quantities (packages_id, quantity) VALUES ((SELECT id FROM packages WHERE name = "${name}"), ${quantities[i]})`, (err, results) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('successfully added to quantities table!');
+          }
+        })
+      }
     }
   });
 })
